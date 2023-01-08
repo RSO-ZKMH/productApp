d6052b94d21f7c476a8cf5ee7bc458d54ce0f8ff
zan.korosak <zan.korosak@xlab.si>
2023-01-08 15:21:55 +0100

    Fixed dockerfile.

--- productApp/vite.config.js
+++ productApp/vite.config.js
@@ -5,11 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 9000,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.APP_BASE_PATH || '/'
+  publicPath: process.env.APP_BASE_PATH || '/',
 })

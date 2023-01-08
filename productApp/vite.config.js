d5b893171c5839f661810d2ffa0ab7226e07dc7f
zan.korosak <zan.korosak@xlab.si>
2023-01-08 15:42:34 +0100

    Added base to config.

--- productApp/vite.config.js
+++ productApp/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const ASSET_URL = process.env.ASSET_URL || '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.APP_BASE_PATH || '/',
+  base: `${ASSET_URL}`,
 })
